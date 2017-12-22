fc0d04d81cbc2c25b02f4820d967b9c2b08304fe
Oliver Phillip Roer <oliver.roer@gmail.com>
2017-12-22 14:10:51 +0100

    revise main

--- main.go
+++ main.go
@@ -1,24 +1,34 @@
 package main
 
-func main() {
-	days := [...]*day{
-		/*
-			day1(),
-			day2(),
-			day3(),
-			day4(),
-			day5(),
-			day6(),
-			day7(),
-			day8(),
-			day9(),
-			day10(),
-			day11(),
-		*/
-		day12(),
-	}
-
+func printAll(days []*day) {
 	for _, day := range days {
 		day.printShort()
 	}
+}
+
+func printLatest(days []*day) {
+	l := len(days)
+	i := l - 1
+	day := days[i]
+	day.printShort()
+}
+
+func main() {
+	days := []*day{
+		day01(),
+		day02(),
+		day03(),
+		day04(),
+		day05(),
+		day06(),
+		day07(),
+		day08(),
+		day09(),
+		day10(),
+		day11(),
+		day12(),
+	}
+
+	//printAll(days)
+	printLatest(days)
 }
